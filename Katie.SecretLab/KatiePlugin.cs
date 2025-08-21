0a825aeb5feb772ba467ca93a193ae38157c7748
Axwabo <axwabo@gmail.com>
2025-08-21 10:06:47 +0200

    init SCP:SL project

---
+++ Katie.SecretLab/KatiePlugin.cs
@@ -0,0 +1,22 @@
+﻿using LabApi.Loader.Features.Plugins;
+
+namespace Katie.SecretLab;
+
+public sealed class KatiePlugin : Plugin
+{
+
+    public override string Name => "K.A.T.I.E.";
+    public override string Description => "Custom announcer";
+    public override string Author => "Axwabo";
+    public override Version Version => GetType().Assembly.GetName().Version;
+    public override Version RequiredApiVersion { get; } = new(1, 0, 0);
+
+    public override void Enable()
+    {
+    }
+
+    public override void Disable()
+    {
+    }
+
+}
