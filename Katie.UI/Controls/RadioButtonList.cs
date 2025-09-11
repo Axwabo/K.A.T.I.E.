fce858fdd84ec094120ce6bf3b6a3fa60c82fbce
Axwabo <axwabo@gmail.com>
2025-09-11 11:38:16 +0200

    vertical signal list

--- Katie.UI/Controls/RadioButtonList.cs
+++ Katie.UI/Controls/RadioButtonList.cs
@@ -1,31 +1,11 @@
-﻿using Avalonia;
-using Avalonia.Controls.Primitives;
-using Avalonia.Controls.Templates;
-using Avalonia.Data;
-using Avalonia.Metadata;
+﻿using Avalonia.Controls.Primitives;
 
 namespace Katie.UI.Controls;
 
 public sealed class RadioButtonList : SelectingItemsControl
 {
 
-    public static readonly StyledProperty<IDataTemplate?> RadioButtonListItemTemplateProperty = AvaloniaProperty.Register<RadioButtonList, IDataTemplate?>(
-        nameof(RadioButtonListItemTemplate),
-        defaultBindingMode: BindingMode.TwoWay,
-        coerce: CoerceSelectionBoxItemTemplate
-    );
-
-    [InheritDataTypeFromItems(nameof(ItemsSource))]
-    public IDataTemplate? RadioButtonListItemTemplate
-    {
-        get => GetValue(RadioButtonListItemTemplateProperty);
-        set => SetValue(RadioButtonListItemTemplateProperty, value);
-    }
-
     public string? GroupName { get; set; }
-
-    private static IDataTemplate? CoerceSelectionBoxItemTemplate(AvaloniaObject obj, IDataTemplate? template)
-        => template is null && obj is RadioButtonList list ? list.ItemTemplate : template;
 
     protected override Control CreateContainerForItemOverride(object? item, int index, object? recycleKey)
         => new RadioButtonListItem(this, item) {GroupName = GroupName, IsChecked = item?.Equals(SelectedItem)};
