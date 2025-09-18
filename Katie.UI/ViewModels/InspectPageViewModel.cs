0d43b99c87215df2d9df66bcfaaf9ea01e54415d
Axwabo <axwabo@gmail.com>
2025-09-18 18:31:18 +0200

    link inspect input to phrases page + default inspect note

--- Katie.UI/ViewModels/InspectPageViewModel.cs
+++ Katie.UI/ViewModels/InspectPageViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.ObjectModel;
 using Avalonia.Layout;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.Input;
 using Katie.Core;
 using Katie.UI.Audio;
-using Katie.UI.Services;
 using NAudio.Wave;
 
 namespace Katie.UI.ViewModels;
@@ -11,13 +11,15 @@ namespace Katie.UI.ViewModels;
 public sealed partial class InspectPageViewModel : ViewModelBase
 {
 
-    private readonly PhraseManager? _manager;
+    private const string DefaultNote = "There may be words that do not map to any phrase. These will be replaced with a pause of (number of characters * 100) milliseconds.";
+
+    public PhrasesPageViewModel PhrasesView { get; }
 
     [ObservableProperty]
-    private string _input = "";
+    [NotifyPropertyChangedFor(nameof(NoteBrush))]
+    private string _note = DefaultNote;
 
-    [ObservableProperty]
-    private string _error = "";
+    public IBrush NoteBrush => Note == DefaultNote ? Brushes.White : Brushes.Red;
 
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(Orientation))]
@@ -27,9 +29,9 @@ public sealed partial class InspectPageViewModel : ViewModelBase
 
     public ObservableCollection<ParsedText> Parsed { get; } = [];
 
-    public InspectPageViewModel(PhraseManager? manager) => _manager = manager;
+    public InspectPageViewModel(PhrasesPageViewModel phrasesView) => PhrasesView = phrasesView;
 
-    public InspectPageViewModel()
+    public InspectPageViewModel() : this(new PhrasesPageViewModel())
     {
         Parsed.Add(new ParsedText("Test", [new UtteranceSegment<WavePhraseBase>(TimeSpan.FromSeconds(0.5), new RawSourceSamplePhrase(new RawSourceSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(48000, 1), [], 0), "testing"))]));
         Parsed.Add(new ParsedText("1", [new UtteranceSegment<WavePhraseBase>(TimeSpan.FromSeconds(0.3), new RawSourceSamplePhrase(new RawSourceSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(48000, 1), [], 0), "one"))]));
@@ -42,13 +44,12 @@ public sealed partial class InspectPageViewModel : ViewModelBase
     [RelayCommand]
     private void Parse(string language)
     {
-        if (_manager == null)
-            return;
         try
         {
-            Error = "";
+            Note = DefaultNote;
             Parsed.Clear();
-            var parser = new PhraseParser<WavePhraseBase>(Input, language == "English" ? _manager.EnglishTree : _manager.HungarianTree);
+            var input = PhrasesView.Text;
+            var parser = new PhraseParser<WavePhraseBase>(input, language == "English" ? PhrasesView.Phrases.EnglishTree : PhrasesView.Phrases.HungarianTree);
             var index = 0;
             var list = new List<UtteranceSegment<WavePhraseBase>>();
             while (parser.Next(out var phrase))
@@ -60,15 +61,15 @@ public sealed partial class InspectPageViewModel : ViewModelBase
                 }
 
                 var start = index;
-                index = phrase.EndIndex == -1 ? Input.Length : phrase.EndIndex;
+                index = phrase.EndIndex == -1 ? input.Length : phrase.EndIndex;
                 list.Add(phrase);
-                Parsed.Add(new ParsedText(Input.Substring(start, index - start), list));
+                Parsed.Add(new ParsedText(input.Substring(start, index - start), list));
                 list = [];
             }
         }
         catch (Exception e)
         {
-            Error = e.Message;
+            Note = e.Message;
         }
     }
 
