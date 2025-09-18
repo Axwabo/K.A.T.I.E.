c69d380769708da88da6edc6d171ce65a55f8152
Axwabo <axwabo@gmail.com>
2025-09-18 07:19:04 +0200

    begin ViewLocator refactor

--- Katie.UI/Extensions/ServiceProviderExtensions.cs
+++ Katie.UI/Extensions/ServiceProviderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Katie.UI.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Katie.UI.Extensions;
 
@@ -10,5 +11,26 @@ internal static class ServiceProviderExtensions
             .AddSingleton<PhrasesPageViewModel>()
             .AddSingleton<CacheManagerViewModel>()
             .AddTransient<SignalsViewModel>();
+
+    private static IServiceCollection AddView<TModel, TView>(this IServiceCollection collection)
+        where TModel : ViewModelBase
+        where TView : UserControl, new()
+        => collection.AddSingleton<TModel>()
+            .AddSingleton<IViewFactory>(new GenericViewFactory<TView>());
+
+    private static IServiceCollection AddTransientView<TModel, TView>(this IServiceCollection collection)
+        where TModel : ViewModelBase
+        where TView : UserControl, new()
+        => collection.AddTransient<TModel>()
+            .AddSingleton<IViewFactory>(new GenericViewFactory<TView>());
+
+}
+
+file sealed class GenericViewFactory<T> : IViewFactory where T : UserControl, new()
+{
+
+    public Type ViewModelType { get; } = typeof(T);
+
+    public Control Build(ViewModelBase viewModel) => new T {DataContext = viewModel};
 
 }
---
+++ Katie.UI/Services/IViewFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Katie.UI.Services;
+
+public interface IViewFactory
+{
+
+    Type ViewModelType { get; }
+
+    Control Build(ViewModelBase viewModel);
+
+}
