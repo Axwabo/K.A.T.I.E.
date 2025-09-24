dbf93b433fe63841f69c32dac780008d65292ff1
Axwabo <axwabo@gmail.com>
2025-09-24 15:51:56 +0200

    copy zip to memory in browser

--- Katie.UI.Browser/BrowserZipOpener.cs
+++ Katie.UI.Browser/BrowserZipOpener.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO.Compression;
 using System.Threading.Tasks;
 using Avalonia.Platform.Storage;
+using Katie.UI.Extensions;
 using Katie.UI.PhraseProviders;
 
 namespace Katie.UI.Browser;
@@ -8,6 +9,11 @@ namespace Katie.UI.Browser;
 public sealed class BrowserZipOpener : IZipOpener
 {
 
-    public async Task<ZipArchive> OpenArchiveAsync(IStorageFile file) => new(new AsyncWrapperStream(await file.OpenReadAsync()));
+    public async Task<ZipArchive> OpenArchiveAsync(IStorageFile file)
+    {
+        await using var stream = await file.OpenReadAsync();
+        var memory = await stream.CopyToMemory();
+        return new ZipArchive(memory);
+    }
 
 }
