7fee6d74affd84399c892181416152ba4d858a0d
Axwabo <axwabo@gmail.com>
2025-09-24 19:30:58 +0200

    cache clearing

--- Katie.UI/ViewModels/CacheManagerViewModel.cs
+++ Katie.UI/ViewModels/CacheManagerViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Katie.NAudio;
 using Katie.UI.PhraseProviders;
 using Katie.UI.Services;
 using Katie.UI.Signals;
 using Microsoft.Extensions.DependencyInjection;
+using NAudio.Wave;
 
 namespace Katie.UI.ViewModels;
 
@@ -61,5 +63,46 @@ public sealed partial class CacheManagerViewModel : ViewModelBase
     private Task LoadArchive() => _zipLoader == null
         ? Task.CompletedTask
         : _zipLoader.LoadPhrasesAsync(Phrases.Hungarian, Phrases.English, Phrases.Global);
+
+    [RelayCommand]
+    private async Task ClearPhraseCache()
+    {
+        if (PhraseCache == null)
+            return;
+        await PhraseCache.ClearAsync();
+        Phrases.Hungarian.UnCache();
+        Phrases.English.UnCache();
+        Phrases.Global.UnCache();
+    }
+
+    [RelayCommand]
+    private Task ClearSignalCache() => SignalCache?.ClearAsync() ?? Task.CompletedTask;
+
+}
+
+file sealed class NotCachedPhrase : WavePhraseBase
+{
+
+    private readonly WavePhraseBase _phrase;
+
+    public NotCachedPhrase(WavePhraseBase phrase) => _phrase = phrase;
+
+    public override string Text => _phrase.Text;
+    public override TimeSpan Duration => _phrase.Duration;
+    public override SimpleWaveFormat WaveFormat => _phrase.WaveFormat;
+    public override ISampleProvider ToSampleProvider() => _phrase.ToSampleProvider();
+
+}
+
+file static class Extensions
+{
+
+    public static void UnCache(this PhrasePackViewModel phrases)
+    {
+        var list = new List<WavePhraseBase>(phrases.List.Count);
+        foreach (var phrase in phrases.List)
+            list.Add(phrase as NotCachedPhrase ?? new NotCachedPhrase(phrase));
+        phrases.ReplacePhrases(list);
+    }
 
 }
